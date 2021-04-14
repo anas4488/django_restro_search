656b6b61e28977399826f4f893b766d2e0cbc5e4
anas4488 <anassayed63@gmail.com>
2021-04-14 15:47:18 +0530

    first commit

--- home/views.py
+++ home/views.py
@@ -1,6 +1,55 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import *
 # Create your views here.
+from geopy.distance import  great_circle
+
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request , 'home.html')
+
+
+
+
+
+def api(request):
+    restaurant_objs = Restaurant.objects.all()
+    
+    pincode = request.GET.get('pincode')
+    km = request.GET.get('km')
+    user_lat = None
+    user_long = None
+    
+    if pincode:
+        geolocator = Nominatim(user_agent="geoapiExercises")
+        location = geolocator.geocode(int(pincode))
+        user_lat = location.latitude
+        user_long = location.longitude 
+    
+    
+    payload = []
+    for restaurant_obj in restaurant_objs:
+        result = {}
+        result['name'] = restaurant_obj.name
+        result['image'] = restaurant_obj.image
+        result['description'] = restaurant_obj.description
+        result['pincode'] = restaurant_obj.pincode
+        if pincode:
+            first = (float(user_lat) , float(user_long))
+            second = (float(restaurant_obj.lat) , float(restaurant_obj.lon))
+            result['distance'] = int( great_circle(first , second).miles)
+        
+        payload.append(result)
+        
+        if km:
+            if result['distance'] > int(km):
+                payload.pop()
+             
+                
+             
+                    
+        
+        
+    return JsonResponse(payload , safe=False)
+    
+    
